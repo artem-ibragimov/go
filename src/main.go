3b93e5348dce6442c4273e794dda08f42f1f7690
artem.ibragimov <artem.ibragimov@gmail.com>
2022-02-07 11:31:39 +0300

    Setup CI

--- src/main.go
+++ src/main.go
@@ -1,37 +1,4 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/elazarl/goproxy"
-)
-
-type IProxyList map[string]string
-type Config struct {
-	Proxy IProxyList `json:"Proxy"`
-}
-
 func main() {
-	var config Config
-	data, err := os.ReadFile("./config.json")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	json.Unmarshal(data, &config)
-	fmt.Println(config)
-
-	proxy := goproxy.NewProxyHttpServer()
-	proxy.Verbose = true
-	log.Fatal(http.ListenAndServe(":8080", proxy))
-
-	proxy.OnRequest().DoFunc(
-		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
-			r.Header.Set("X-GoProxy", "yxorPoG-X")
-			return r, nil
-		})
 }
