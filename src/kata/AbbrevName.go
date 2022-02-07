d0a48f28f729298035457de7edff77051209618a
artem.ibragimov <artem.ibragimov@gmail.com>
2022-02-07 11:02:24 +0300

    Abbreviate a Two Word Name
    https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/go

--- src/kata/AbbrevName.go
+++ src/kata/AbbrevName.go
@@ -2,6 +2,8 @@ package main
 
 import "strings"
 
+// Abbreviate a Two Word Name
+// https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/train/go
 // Write a function to convert a name into initials.
 // This kata strictly takes two words with one space in between them.
 // The output should be two capital letters with a dot separating them.
